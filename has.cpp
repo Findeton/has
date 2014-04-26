871835cf8acc043f16f0c1cb659f134e24fa76f5
Findeton <felrobelv@gmail.com>
2014-04-26 21:49:33 +0200

    add first snippet of code

---
+++ has.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : has.cpp
+// Author      : Felix Robles felrobelv at gmail dot com
+// Version     :
+// Copyright   : GPL Affero v3
+// Description : has OpenRISC Assembler
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
